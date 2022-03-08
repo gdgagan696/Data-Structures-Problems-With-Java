8042c0f1edff6fd02c1aec993bdc09466dc5b228
gdgagan696 <gdgagan696@gmail.com>
2022-03-08 08:53:43 +0530

    Leetcode daily challenge-LinkedList Cycle

---
+++ src/dsquestions/linkedlist/LinkedListCycle.java
@@ -0,0 +1,20 @@
+package dsquestions.linkedlist;
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

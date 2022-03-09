ee8231f5b54a6ef0c55e3fffaafc896fb4926488
gdgagan696 <gdgagan696@gmail.com>
2022-03-09 09:13:57 +0530

    Leetcode daily challenge-RemoveDuplicatesSortedListII

---
+++ src/dsquestions/linkedlist/RemoveDuplicatesSortedListII.java
@@ -0,0 +1,24 @@
+package dsquestions.linkedlist;
+
+public class RemoveDuplicatesSortedListII {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.next != null && curr.val == curr.next.val) {
+                while (curr.next != null && curr.val == curr.next.val) {
+                    curr = curr.next;
+                }
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+
+    }
+}

af3bf56227393b6cccfa4f648c397caaec731519
gdgagan696 <gdgagan696@gmail.com>
2022-03-30 09:08:31 +0530

    Leetcode daily challenge-SearchMatrix

--- src/dsquestions/arrays/SearchMatrix.java
+++ src/dsquestions/arrays/SearchMatrix.java
@@ -2,7 +2,7 @@ package dsquestions.arrays;
 
 public class SearchMatrix {
 
-    private boolean searchMatrix(int[][] matrix, int target) {
+    public static boolean searchMatrix(int[][] matrix, int target) {
 
         boolean isFound = false;
         for (int i = 0; i < matrix.length; i++) {
@@ -15,7 +15,7 @@ public class SearchMatrix {
 
     }
 
-    private boolean binarySearch(int[] matrix, int target) {
+    private static boolean binarySearch(int[] matrix, int target) {
         int l = 0;
         int h = matrix.length - 1;
         while (l <= h) {
@@ -32,7 +32,28 @@ public class SearchMatrix {
 
     }
 
+    public static boolean searchMatrix_2(int[][] matrix, int target) {
+
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+
+    }
+
     public static void main(String[] args) {
+        int mat[][] = {
+                {1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}
+        };
+        System.out.println(searchMatrix_2(mat, 3));
 
     }
 }

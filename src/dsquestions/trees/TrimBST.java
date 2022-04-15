b17a401aacfa8dbe4348fa364b6b96b03aa70f8b
gdgagan696 <gdgagan696@gmail.com>
2022-04-15 12:31:27 +0530

    Leetcode daily challenge-TrimBST

--- src/dsquestions/trees/TrimBST.java
+++ src/dsquestions/trees/TrimBST.java
@@ -2,12 +2,12 @@ package dsquestions.trees;
 
 public class TrimBST {
     public TreeNode trimBST(TreeNode root, int low, int high) {
-        if(root == null) return root;
+        if (root == null) return root;
         root.left = trimBST(root.left, low, high);
         root.right = trimBST(root.right, low, high);
 
-        if(root.val < low) return root.right;
-        else if(root.val > high) return root.left;
+        if (root.val < low) return root.right;
+        else if (root.val > high) return root.left;
         return root;
     }
 }

76fe1e7bdeb85e835a3a5bc6c4f8d9eb35ba4e1e
gdgagan696 <gdgagan696@gmail.com>
2022-05-13 09:52:53 +0530

    Leetcode daily challenge-PopulatingNextRightPointersInEachNodeII

---
+++ src/dsquestions/trees/PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,32 @@
+package dsquestions.trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PopulatingNextRightPointersInEachNodeII {
+    public TreeNode connect(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i != size - 1) {
+                    node.next = queue.peek();
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return root;
+
+    }
+}

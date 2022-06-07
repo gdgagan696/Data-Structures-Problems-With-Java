c145c46bf2529df6ca51f2aa210c6e90b6ff2edf
gdgagan696 <gdgagan696@gmail.com>
2022-06-07 08:34:20 +0530

    Leetcode daily challenge-MergeSortedArray

--- src/dsquestions/arrays/MergeSortedArray.java
+++ src/dsquestions/arrays/MergeSortedArray.java
@@ -9,11 +9,10 @@ public class MergeSortedArray {
         int j = n - 1;
         while (k >= 0 && j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i--];
+                nums1[k--] = nums1[i--];
             } else {
-                nums1[k] = nums2[j--];
+                nums1[k--] = nums2[j--];
             }
-            k--;
         }
 
     }

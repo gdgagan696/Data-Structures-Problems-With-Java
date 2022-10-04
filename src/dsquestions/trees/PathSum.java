73e68008b30d2a0be9fd4b0f0f8c854077901f17
gdgagan696 <gdgagan696@gmail.com>
2022-10-04 08:39:49 +0530

    Leetcode daily challenge-PathSum

---
+++ src/dsquestions/trees/PathSum.java
@@ -0,0 +1,13 @@
+package dsquestions.trees;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            return true;
+        }
+        boolean left = hasPathSum(root.left, targetSum - root.val);
+        boolean right = hasPathSum(root.right, targetSum - root.val);
+        return left || right;
+    }
+}

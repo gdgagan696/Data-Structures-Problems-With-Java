0c06baee69cc018670c92a734a4ceb244d9a0a38
gdgagan696 <gdgagan696@gmail.com>
2022-10-12 09:24:01 +0530

    Leetcode daily challenge-LargestPerimeterTriangle

---
+++ src/dsquestions/arrays/LargestPerimeterTriangle.java
@@ -0,0 +1,19 @@
+package dsquestions.arrays;
+
+import java.util.Arrays;
+
+public class LargestPerimeterTriangle {
+    public static int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i > 1; i--) {
+            if (nums[i] < nums[i - 1] + nums[i - 2])
+                return nums[i] + nums[i - 1] + nums[i - 2];
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(largestPerimeter(new int[]{2,1,2}));
+    }
+
+}

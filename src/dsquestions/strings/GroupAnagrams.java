6dad8bfc57b3dab0927347bcfab5e8a35c1580d9
gdgagan696 <gdgagan696@gmail.com>
2022-10-28 14:06:33 +0530

    Leetcode daily challenge-GroupAnagrams

---
+++ src/dsquestions/strings/GroupAnagrams.java
@@ -0,0 +1,31 @@
+package dsquestions.strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramsMappings = new HashMap<>();
+        for (String currentStr : strs) {
+            char[] currentStrArray = currentStr.toCharArray();
+            Arrays.sort(currentStrArray);
+            String sortedString = new String(currentStrArray);
+            anagramsMappings.compute(sortedString, (key, value) -> {
+                if (value == null) {
+                    value = new ArrayList<>();
+                }
+                value.add(currentStr);
+                return value;
+            });
+        }
+        return new ArrayList<>(anagramsMappings.values());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
+}
+
